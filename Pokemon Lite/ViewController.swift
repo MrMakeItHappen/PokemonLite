0fd10b61d497eec8241ed69607fa74ac75d46eac
Sdot <user_01@Air-Bender.local>
2017-01-11 17:40:51 -0500

    Map Kit installed. Center icon enabled.

--- Pokemon Lite/ViewController.swift
+++ Pokemon Lite/ViewController.swift
@@ -7,18 +7,65 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var manager = CLLocationManager()
+    var updateCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        manager.delegate = self
+        
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            mapView.showsUserLocation = true
+            manager.startUpdatingLocation()
+            
+            Timer.scheduledTimer(withTimeInterval: 5, repeats: true, block: { (timer) in
+                
+                if let coord = self.manager.location?.coordinate {
+                    let annotation = MKPointAnnotation()
+                    annotation.coordinate = coord
+                    self.mapView.addAnnotation(annotation)
+                
+                }
+                
+                
+            })
+            
+            
+            
+        } else {
+            manager.requestWhenInUseAuthorization()
+        }
+      
+        
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if updateCount < 1 {
+            let region = MKCoordinateRegionMakeWithDistance(manager.location!.coordinate, 400, 400)
+            mapView.setRegion(region, animated: false)
+            updateCount += 1
+        } else {
+            manager.stopUpdatingLocation()
+        }
+    }
+    
+    @IBAction func centerTapped(_ sender: Any) {
+        
+        if let coordinate = manager.location?.coordinate {
+            let region = MKCoordinateRegionMakeWithDistance(coordinate, 400, 400)
+            mapView.setRegion(region, animated: true)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
